6cd2e91b6da30c199bad03e6409beb753d61a48b
pobear <pobear@126.com>
2023-03-11 16:23:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     install_requires=[
         "numpy>=1.22.3",
         "pandas",
-        "PyQt5",
+        "PyQt6",
         "pyqtgraph>=0.11.1",
         "python-dateutil",
     ],
